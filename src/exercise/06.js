142453eb76ef801c589f49b63c0518af6693dc7c
Nathan Ogden <nathan.ogden@birdtronics.net>
2023-06-02 18:50:26 +0800

    E6: Bonus 8

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -85,7 +85,7 @@ function App() {
       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
       <hr />
       <div className="pokemon-info">
-        <ErrorBoundary onReset={() => setPokemonName('')}FallbackComponent={ErrorFallback}>
+        <ErrorBoundary resetKeys={[pokemonName]} onReset={() => setPokemonName('')}FallbackComponent={ErrorFallback}>
           <PokemonInfo pokemonName={pokemonName} />
         </ErrorBoundary>
       </div>
